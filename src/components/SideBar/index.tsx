6d506aedeaa37c63e3e06509e8ed9aeb2ce405e9
Kim <kim.delrosario@respiri.co>
2023-11-26 17:39:16 +0800

    Update

--- src/components/SideBar/index.tsx
+++ src/components/SideBar/index.tsx
@@ -1,20 +1,54 @@
 'use client';
 
 import { AppContext } from '@/contexts/App';
-import { Typography } from '@mui/material';
+import CloseIcon from '@mui/icons-material/Close';
+import Box from '@mui/material/Box';
 import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
 import { useContext } from 'react';
 
 export default function SideBar() {
   const { showDrawer, setShowDrawer } = useContext(AppContext);
 
+  const toggleDrawer = () => setShowDrawer((prevShowDrawer) => !prevShowDrawer);
+
   return (
     <Drawer
       anchor="right"
       open={showDrawer}
-      onClose={() => setShowDrawer((prevShowDrawer) => !prevShowDrawer)}
+      onClose={toggleDrawer}
+      PaperProps={{
+        sx: {
+          width: {
+            xs: 300,
+            sm: '50%',
+            md: '40%',
+          },
+          px: {
+            xs: 5,
+            lg: 10,
+          },
+          py: {
+            xs: 4,
+            lg: 7,
+          },
+        },
+      }}
     >
       <Typography>Test</Typography>
+
+      <Box
+        sx={{
+          position: 'absolute',
+          top: (theme) => theme.spacing(3),
+          right: (theme) => theme.spacing(2.5),
+        }}
+      >
+        <IconButton onClick={toggleDrawer}>
+          <CloseIcon sx={{ fontSize: (theme) => theme.spacing(3.5) }} />
+        </IconButton>
+      </Box>
     </Drawer>
   );
 }
