d7dc31d3115762b5d8fe41930da9eac5d25c2460
kimdelrosario006 <KDelRosario@jaycar.com.au>
2025-09-12 23:11:27 +0800

    Added Tawk.to

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { PropsWithChildren } from 'react';
 
 import Main from '@/containers/Main';
 import { Metadata } from 'next';
+import Script from 'next/script';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -79,6 +80,21 @@ export default function RootLayout({ children }: PropsWithChildren) {
       </head>
       <body id="main">
         <Main>{children}</Main>
+
+        {/* Tawk.to Script */}
+        <Script id="tawk-to" strategy="afterInteractive">
+          {`
+            var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+            (function(){
+            var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+            s1.async=true;
+            s1.src='https://embed.tawk.to/${process.env.NEXT_PUBLIC_TAWKTO_PROPERTY_ID}/${process.env.NEXT_PUBLIC_TAWKTO_WIDGET_ID}';
+            s1.charset='UTF-8';
+            s1.setAttribute('crossorigin','*');
+            s0.parentNode.insertBefore(s1,s0);
+            })();
+          `}
+        </Script>
       </body>
     </html>
   );
